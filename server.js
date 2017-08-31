e1d560dcc82912c99aae80fc3dcc0aea26cf5c89
sb4singh <sb4singh@gmail.com>
2017-08-31 23:19:55 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+
+global.port = 1200;
+
+global.server = app.listen(global.port, (resp) => {
+    console.log('server running on port: ' + global.port);
+});
+
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    limit: '100mb',
+    extended: true
+}));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.post('/facebook-notification', () => {
+    console.log(req.body);
+})
