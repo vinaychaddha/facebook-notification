0a61972784547f4d1ccb041e5728c468d05fd904
sb4singh <sb4singh@gmail.com>
2017-09-01 00:25:14 +0530

    Add files via upload

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,4 @@
-$.views.helpers({
-    moment: function(r, format){
-        return moment(r).format(format);
-    },
-    toFixed : function(r){
-        return (1 * r).toFixed(2);
-    },
-    lowerCase: function(r){
-        return r.toLowerCase();
-    } 
-})
-
-var id;
 
 $(document).ready(function () {
-    makeTemplates();
-    inputScreen.show();
+    console.log('I AM READY TO RECEIVE TO POSTS :-)');
 })
